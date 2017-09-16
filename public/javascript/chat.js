df9eb778db7230a1e164fa0853c2f6fa3852ab89
siddesh.angadi5@gmail.com <siddesh.angadi5@gmail.com>
2017-09-16 07:52:23 +0000

    lunch

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -2,4 +2,27 @@ var app = angular.module('chatApp',['ngMaterial']);
 
 app.controller('chatController',function ($scope) {
 
+	$scope.messages = [
+	{
+		sender: "BOT",
+		text :"what can I do for you?",
+		time : "06.45pm"
+	},
+	{
+		sender:"USER",
+		text:"what 1+1",
+		time : "06.48am"
+	},
+	{
+		sender:"BOT",
+		text:"2",
+		time : "06.50pm"
+	},
+	];
+var  exampleSocket =  new  WebSocket("ws://localhost:9000/chatSocket");
+exampleSocket.onmessage  =   function  (event) {
+       var jsonData = JSON.parse(event.data);
+       console.log(jsonData);
+   };
+
 });
